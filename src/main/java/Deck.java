75a00eda390606faa3c2e0309517aa40ba9822b3
Graeme Brown <graemehbrown@googlemail.com>
2018-01-10 14:54:04 +0000

    Populated deck with cards.  Created method to return deck.

--- src/main/java/Deck.java
+++ src/main/java/Deck.java
@@ -11,4 +11,17 @@ public class Deck {
     public int cardCount() {
         return this.deck.size();
     }
+
+    public void fillDeck() {
+        for (Suit suit: Suit.values()){
+            for (Rank rank: Rank.values()){
+                Card cardToAdd = new Card(suit, rank);
+                deck.add(cardToAdd);
+            }
+        }
+    }
+
+    public ArrayList getDeck(){
+        return this.deck;
+    }
 }
--- src/test/java/DeckTest.java
+++ src/test/java/DeckTest.java
@@ -1,4 +1,5 @@
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -6,15 +7,29 @@ import static org.junit.Assert.assertEquals;
 public class DeckTest {
 
     Deck deck;
+    Card card;
 
     @Before
     public void before(){
         deck = new Deck();
+        card = new Card(Suit.SPADES, Rank.ACE);
     }
 
     @Test
     public void deckStartsEmpty(){
         assertEquals(0, deck.cardCount());
+    }
+
+    @Test
+    public void deckHasCards(){
+        deck.fillDeck();
+        assertEquals(52, deck.cardCount());
+    }
+
+    @Ignore//can't easily compare
+    public void deckContainsCard(){
+        deck.fillDeck();
+        assertEquals(true, deck.getDeck().equals(card));
     }
 
 }
